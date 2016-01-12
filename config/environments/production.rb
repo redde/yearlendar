87c82700b2ff0a865313d3010796629f448819bf
Oleg Bovykin <oleg.bovykin@gmail.com>
2016-01-12 15:08:50 +0300

    Правильный кеш для пумы

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ Rails.application.configure do
                      :password => ENV["MEMCACHIER_PASSWORD"],
                      :failover => true,
                      :socket_timeout => 1.5,
-                     :socket_failure_delay => 0.2
+                     :socket_failure_delay => 0.2,
+                     :pool_size => 5
                     }
 
   # Disable serving static files from the `/public` folder by default since
